d2e8bae2b0b05fed9023a83be12c1094fdce6bb4
W3C Digital Network <w3cdigitalnetwork@gmail.com>
2026-09-09 12:49:59 +0000

    Restore and standardize remaining back buttons

--- artifacts/pibooking/src/components/PublicProfileView.tsx
+++ artifacts/pibooking/src/components/PublicProfileView.tsx
@@ -1,1 +1,5 @@
 import React, { useState } from 'react';
+import { BusinessProfile, Provider, Service, SocialLink } from '../types';
+import { X, MessageSquare, BadgeCheck } from 'lucide-react';
+import { providerMediaService } from '../services/providerMediaService';
+import { BackButton } from './BackButton';
