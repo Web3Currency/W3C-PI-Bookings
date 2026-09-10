dd975ebbf3088f2d7348c77ecbd591fd032bfadf
W3C Digital Network <w3cdigitalnetwork@gmail.com>
2026-09-10 18:22:13 +0000

    Add Vercel Speed Insights

--- artifacts/pibooking/src/main.tsx
+++ artifacts/pibooking/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import App from './App';
 import { Toaster } from './components/ui/toaster';
 import './index.css';
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
     <Toaster />
+    <SpeedInsights />
   </StrictMode>,
 );
